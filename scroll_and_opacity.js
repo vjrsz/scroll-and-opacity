23f63aa280005cba5e345c8ab6804fb551ebff11
vjrsz <juniornunes1008@gmail.com>
2022-08-24 08:23:43 -0300

    complete

--- scroll_and_opacity.js
+++ scroll_and_opacity.js
@@ -1,10 +1,28 @@
 $(document).scroll(function(){
-    el = $("#elementId")[0];
+    var elements = document.getElementsByClassName("os__text-item")
 
-    if ( el.getBoundingClientRect().top < window.innerHeight/2){
-        el.classList.add("blur");
+    var main = 0;
+    main = elements[0];
 
-    }else{
-        el.classList.remove("blur");
+    for (let el of elements) {
+        console.log(el.getBoundingClientRect().top, window.innerHeight/2 )
+
+        let topEl = el.getBoundingClientRect().top
+        let topMain = main.getBoundingClientRect().top
+
+        if ( topEl < window.innerHeight/2  &&
+            topEl > window.innerHeight/2 -200
+        ){
+            main = topEl > topMain ? el : main;
+            // el.classList.add("blur");
+        }else{
+            el.classList.remove("blur");
+        }
+    };
+
+    main.classList.add("blur");
+
+    for(let el of elements) {
+        el != main ? el.classList.remove('blur') : false;
     }
 })
